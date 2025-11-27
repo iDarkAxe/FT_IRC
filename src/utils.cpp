48111dbd167a33b352c73f6481193758f6579043
Pierre Pontet <ppontet@student.42lyon.fr>
2025-11-27 18:56:56 +0100

    fix: stat forbidden, using fstat with fd instead

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,34 +2,34 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-static bool is_terminal(const char *path);
+static bool is_terminal(int fd);
 
 bool is_stdout_on_tty()
 {
-	return is_terminal("/dev/stdout");
+	return is_terminal(1);
 }
 
 bool is_stderr_on_tty()
 {
-	return is_terminal("/dev/stderr");
+	return is_terminal(2);
 }
 
 bool is_stdin_on_tty()
 {
-	return is_terminal("/dev/stdin");
+	return is_terminal(0);
 }
 
 /**
  * @brief Check if a given path is a terminal
  * 
- * @param[in] path path to check (e.g., "/dev/stdout") 
+ * @param[in] fd file descriptor to check 
  * @return true true if the path is a terminal
  * @return false false if the path is not a terminal
  */
-bool is_terminal(const char* path)
+bool is_terminal(int fd)
 {
 	struct stat st;
-	if (stat(path, &st) == -1)
+	if (fstat(fd, &st) == -1)
 		return false;
 	return S_ISCHR(st.st_mode);
 }
