11a1af94eae5685b25912f6aaf36368da60eaaca
muffin <muffin@arch.man>
2025-11-27 19:00:24 +0100

    blueprint PassCommand

---
+++ src/PassCommand.cpp
@@ -0,0 +1,68 @@
+#include "PassCommand.hpp"
+
+PassCommand::PassCommand(std::vector<std::string> params)
+{
+	_params = params;
+}
+
+void PassCommand::execute(Client* executor, NetworkState& network)
+{
+  //Comment traiter PASS password extra params ?
+	if (_params.size() < 1) {
+		// ERR_NEEDMOREPARAMS
+		return;
+	}
+	Client* password = network.getClient(_params[0]);
+    if (!target || !channel) {
+		// ERR_NOSUCHNICK
+		Debug::print(DEBUG, "Invalid params");
+        // executor.sendError("Invalid params");
+        return;
+    }
+	if (!channel->isClientInChannel(executor))
+	{
+		// ERR_NOTONCHANNEL
+		Debug::print(DEBUG, "Client is not in channel");
+		// executor.sendError("Client is not in channel");
+		return;
+	}
+	if (channel->isClientInChannel(target))
+	{
+		// ERR_USERONCHANNEL
+		Debug::print(DEBUG, "Target user is already in channel");
+		// executor.sendError("Target user is already in channel");
+		return;
+	}
+	if (channel->getModes().is_invite_only)
+	{
+		if (channel->isClientOPChannel(executor))
+		{
+			if (!target)
+			{
+				// RPL_AWAY
+				return;
+			}
+			// RPL_INVITING
+			channel->addClient(target);
+			return;
+		}
+		else
+		{
+			// ERR_CHANOPRIVSNEEDED
+			Debug::print(DEBUG, "Client is not channel operator");
+			// executor.sendError("Client is not channel operator");
+			return;
+		}
+	}
+	else // Not invite only
+	{
+		if (!target)
+		{
+			// RPL_AWAY
+			return;
+		}
+		// RPL_INVITING
+		channel->addClient(target);
+		return;
+	}
+}
