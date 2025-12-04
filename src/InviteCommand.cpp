44f14c704c78fd9eba57071e950c1aff3fd2a492
Olivier Elleaume <oelleaum@z4r2p7.42lyon.fr>
2025-12-04 18:02:36 +0100

    invite

--- src/InviteCommand.cpp
+++ src/InviteCommand.cpp
@@ -11,11 +11,8 @@ void InviteCommand::execute(Client* executor, Server& server)
 		server.reply(executor, ERR_NEEDMOREPARAMS(executor->getNickname(), "INVITE"));
 		return;
 	}
-	Client* target = NULL; // a virer
-	//ajouter fd en param et chercher dans la map
-	// Client* target = server.getClient(_params[0]);
-	Channel* channel = NULL;
-	// Channel* channel = server.getChannel(_params[1]);
+	Client* target = server.getClient(_params[0]);
+	Channel* channel = server.getChannel(_params[1]);
 	if (!target || !channel) {
 		server.reply(executor, ERR_NOSUCHNICK(executor->getNickname(), _params[1]));
 		return;
