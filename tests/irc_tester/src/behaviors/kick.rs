07724c35cb04c3cf71a66f8d97b563782e2ff2b7
Olivier Elleaume <oelleaum@z4r9p4.42lyon.fr>
2025-12-06 20:36:03 +0100

    kick refacto

--- tests/irc_tester/src/behaviors/kick.rs
+++ tests/irc_tester/src/behaviors/kick.rs
@@ -9,37 +9,16 @@ pub async fn kick_need_more_params(port: u16, id: usize, timeout_ms: u64) -> Res
     let nick = format!("kickneedmorep_{}", id);
     let mut client = Client::connect(port).await?;
 
-    client.send("PASS password\r\n", 0).await?;
-    client
-        .send(&format!("NICK {}\r\n", nick), 0)
-        .await?;
-    client
-        .send(
-            &format!(
-                "USER {} 0 * :kick_need_more_params\r\n",
-                nick
-            ),
-            0,
-        )
-        .await?;
+    client.authenticate(nick, timeout_ms).await?;
+    client.try_expect("KICK \r\n", " :Not enough parameters", "ERR_NEEDMOREPARAMS missing ", timeout_ms).await?;
 
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if !line.contains("Welcome to the Internet Relay Network") {
-            return Err(anyhow::anyhow!("welcome message missing | received [{line}]"));
-        }
-    }
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if line.starts_with("PING") {
-            let resp = line.replace("PING", "PONG");
-            client.send(&resp, 0).await?;
-        }
-    }
-    client.send("KICK \r\n", 0).await?;
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if !line.contains(" :Not enough parameters") {
-            return Err(anyhow::anyhow!("ERR_NEEDMOREPARAMS missing | received [{line}]"));
-        }
-    }
+    // if let Some(line) = client.read_line_timeout(timeout_ms).await? {
+    //     if line.starts_with("PING") {
+    //         let resp = line.replace("PING", "PONG");
+    //         client.send(&resp, 0).await?;
+    //     }
+    // }
+    
     client.shutdown().await?;
     Ok(())
 }
@@ -48,36 +27,15 @@ pub async fn kick_no_such_channel(port: u16, id: usize, timeout_ms: u64) -> Resu
     let channel = format!("Kick_no_such_chan_{}", id);
     let mut client = Client::connect(port).await?;
 
-    client.send("PASS password\r\n", 0).await?;
-    client
-        .send(&format!("NICK {}\r\n", channel), 0)
-        .await?;
-    client
-        .send(
-            &format!("USER {} 0 * :kick_ns_chan\r\n", channel),
-            0,
-        )
-        .await?;
+    client.authenticate(nick, timeout_ms).await?;
+    client.try_expect("KICK #nosuchchannel user\r\n", " :No such channel", "ERR_NOSUCHCHAN missing ", timeout_ms).await?;
 
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if !line.contains("Welcome to the Internet Relay Network") {
-            return Err(anyhow::anyhow!("welcome message missing | received [{line}]"));
-        }
-    }
-
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if line.starts_with("PING") {
-            let resp = line.replace("PING", "PONG");
-            client.send(&resp, 0).await?;
-        }
-    }
-
-    client.send("KICK #nosuchchannel user\r\n", 0).await?;
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if !line.contains(" :No such channel") {
-            return Err(anyhow::anyhow!("ERR_NOSUCHCHAN missing | received [{line}]"));
-        }
-    }
+    // if let Some(line) = client.read_line_timeout(timeout_ms).await? {
+    //     if line.starts_with("PING") {
+    //         let resp = line.replace("PING", "PONG");
+    //         client.send(&resp, 0).await?;
+    //     }
+    // }
     client.shutdown().await?;
     Ok(())
 }
--- tests/irc_tester/src/behaviors/nick.rs
+++ tests/irc_tester/src/behaviors/nick.rs
@@ -4,29 +4,8 @@ use anyhow::Result;
 pub async fn nick_normal_claim_and_change(port: u16, id: usize, timeout_ms: u64) -> Result<()> {
     let nick = format!("nick_change_{}", id);
     let mut client = Client::connect(port).await?;
-
-    client.send("PASS password\r\n", 0).await?;
-    client
-        .send(&format!("NICK {}\r\n", nick), 0)
-        .await?;
-    client
-        .send(&format!("USER {} 0 * :NoNick\r\n", nick), 0)
-        .await?;
-
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if !line.contains("Welcome to the Internet Relay Network") {
-            return Err(anyhow::anyhow!("Welcome message missing | received [{line}]"));
-        }
-    }
-
-    client
-        .send(&format!("NICK {}_claimed\r\n", nick), 0)
-        .await?;
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if !line.contains("updated his nickname to") {
-            return Err(anyhow::anyhow!("failed to update nickname | received [{line}]"));
-        }
-    }
+    client.authenticate(nick, timeout_ms).await?;
+    client.try_expect("NICK {}_claimed\r\n", "updated his nickname to", "Failed to update nickname ", timeout_ms).await?;
     client.shutdown().await?;
     Ok(())
 }
@@ -34,27 +13,8 @@ pub async fn nick_normal_claim_and_change(port: u16, id: usize, timeout_ms: u64)
 pub async fn nick_no_nickname_given(port: u16, id: usize, timeout_ms: u64) -> Result<()> {
     let nick = format!("nonickgiven_{}", id);
     let mut client = Client::connect(port).await?;
-
-    client.send("PASS password\r\n", 0).await?;
-    client
-        .send(&format!("NICK {}\r\n", nick), 0)
-        .await?;
-    client
-        .send(&format!("USER {} 0 * :NoNick\r\n", nick), 0)
-        .await?;
-
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if !line.contains("Welcome to the Internet Relay Network") {
-            return Err(anyhow::anyhow!("Welcome message missing | received [{line}]"));
-        }
-    }
-
-    client.send("NICK\r\n", 0).await?;
-    if let Some(line) = client.read_line_timeout(timeout_ms).await? {
-        if !line.contains("No nickname given") {
-            return Err(anyhow::anyhow!("failed to reject update | received [{line}]"));
-        }
-    }
+    client.authenticate(nick, timeout_ms).await?;
+    client.try_expect("NICK\r\n", "No nickname given", "Failed to reject update ", timeout_ms).await?;
     client.shutdown().await?;
     Ok(())
 }
