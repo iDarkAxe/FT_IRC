daac392dca3457f06472f31e73638bcb74e5aaec
Pierre Pontet <ppontet@student.42lyon.fr>
2025-12-13 19:16:20 +0100

    fix: replaced kitty by terminator

--- bonus/run_bot.sh
+++ bonus/run_bot.sh
@@ -1,50 +1,54 @@
 #!/bin/bash
 
-
-kill_kitties() {
-    echo "Nettoyage en cours..."
-    
-    pkill -f "kitty --title \"IRC Server\"" 2>/dev/null || true
-    pkill -f "kitty --title \"IRC Client\"" 2>/dev/null || true
-    pkill -f "kitty --title \"Bots\"" 2>/dev/null || true
-    
-    sleep 1
-}
-trap kill_kitties EXIT
-
-if ! command -v kitty &>/dev/null; then
-    echo "Error : Kitty not found"
+if ! command -v terminator &>/dev/null; then
+    echo "Error : Terminator not found"
     exit 1
 fi
+
+kill_terminals() {
+    echo "Erasing..."
+    
+    kill -9 $TERMINATOR_PID_SERV 2>/dev/null || true
+    kill -9 $TERMINATOR_PID_PLAYER 2>/dev/null || true
+    kill -9 $TERMINATOR_PID_BOTS 2>/dev/null || true
+
+    sleep 1
+}
+
+# trap kill_terminals EXIT
 
 PORT=6667
 PASSWORD="password"
 NICKNAME="Player"
 USERNAME="Player"
-KITTY_PID_SERV=0
-KITTY_PID_PLAYER=0
-KITTY_PID_BOTS=0
+TERMINATOR_PID_SERV=0
+TERMINATOR_PID_PLAYER=0
+TERMINATOR_PID_BOTS=0
 
-kitty --title "IRC Server" --hold bash -c "make && ./ircserv 6667 password" &
-KITTY_PID_SERV=$!
-echo "Serveur IRC lancé dans Kitty (PID: $KITTY_PID_SERV)"
-
-sleep 2
-
-kitty --title "Bots" --hold bash -c "cd bonus/bot && cargo run" &
-KITTY_PID_BOTS=$!
-echo "Bots lancés dans Kitty (PID: $KITTY_PID_BOTS)"
+terminator --title "IRC Server" -e 'bash -c "make && ./ircserv 6667 password"' 2>/dev/null &
+TERMINATOR_PID_SERV=$!
+echo "IRC Server started in Terminator (PID: $TERMINATOR_PID_SERV)"
 
 sleep 2
 
-kitty --title "IRC Client" --hold sh -c "
-    echo 'Connexion au serveur IRC sur port $PORT...'
-    {
-        echo 'PASS $PASSWORD'
-        echo 'NICK $NICKNAME'
-        echo 'USER $USERNAME 0 0 0 0'
-        cat
-    } | nc -C 127.0.0.1 $PORT
-" &
-KITTY_PID_PLAYER=$!
+terminator --title "Bots" -e 'bash -c "cd bonus/bot && cargo run"' 2>/dev/null &
+TERMINATOR_PID_BOTS=$!
+echo "Bots started in Terminator (PID: $TERMINATOR_PID_BOTS)"
 
+sleep 2
+
+TEMP_SCRIPT=$(mktemp)
+cat > "$TEMP_SCRIPT" << EOF
+#!/bin/sh
+echo 'Connexion au serveur IRC sur port $PORT...'
+{
+    echo 'PASS $PASSWORD'
+    echo 'NICK $NICKNAME'
+    echo 'USER $USERNAME 0 0 0 0'
+    cat
+} | nc -C 127.0.0.1 $PORT
+EOF
+
+chmod +x "$TEMP_SCRIPT"
+terminator --title="IRC Client" -e "$TEMP_SCRIPT" &
+TERMINATOR_PID_PLAYER=$!
